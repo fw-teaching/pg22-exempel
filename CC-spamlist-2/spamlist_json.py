dda2d90ddd3462b1142aa00455280bf5fe110cd0
Fredde <welandfr@arcada.fi>
2022-12-14 13:22:42 +0200

    Mellancommit

---
+++ CC-spamlist-2/spamlist_json.py
@@ -0,0 +1,22 @@
+import json
+
+with open('emails.json') as f:
+    marketing_data_json = f.read()
+
+emails_list = json.loads(marketing_data_json) # konvertera JSON till python-objekt
+fixed_emails_list = [] # Skapa tom lista för de korrekta och fixade email-adresserna
+
+for email in emails_list: # Loopa igenom hela marketing_data-listan
+  email = email.lower().strip() # Ändra vaje till små bokstäver och ta bort onödiga mellanslag och radbryt
+
+  if "@arcada.fi" in email: # Finns @arcada.fi i den email vi just nu kollar?
+    fixed_emails_list.append(email) # Lägg till vår fixade lista
+
+fixed_emails_list.sort() # Sortera i alfabetisk ordning
+
+fixed_emails_json = json.dumps(fixed_emails_list, indent=2)
+
+print(fixed_emails_json)
+
+with open('refined_emails.json', 'w') as f:
+    f.write(fixed_emails_json)
